e2e319f759e4922c24c2134e4d3e029963d65497
Ayaz-Ahamed-Shaik <ayaz55ahammad@gmail.com>
2025-04-23 13:32:58 +0530

    frontend

---
+++ server/models/employeejobpostmodel.js
@@ -0,0 +1,33 @@
+import  mongoose from 'mongoose';
+const employeepostSchema=mongoose.Schema({
+   jobtitle:{
+    type:String,
+    required:true,
+   },
+   jobdescription:{
+    type:String,
+    required:true,
+   },
+   companyname:{
+    type:String,
+    required:true,
+   },
+   location:{
+    type:String,
+    required:true,
+   },
+   jobtype:{
+    type:String,
+    required:true,
+   },
+   experiencelevel:{
+    type:String,
+    required:true,
+   },
+   dataposted:{
+    type:Date,
+    default:Date.now()
+   }
+});
+const employeejobmodel=mongoose.model('employeepostmodel',employeepostSchema);
+export default employeejobmodel;
