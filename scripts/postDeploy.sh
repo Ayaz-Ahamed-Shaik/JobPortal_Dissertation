9a7ea0ed40202fe4acaaca9de29cb9a1a9b0c816
Ayaz-Ahamed-Shaik <ayaz55ahammad@gmail.com>
2025-06-09 13:08:34 +0530

    corrections

--- scripts/postDeploy.sh
+++ scripts/postDeploy.sh
@@ -3,8 +3,8 @@
 # Go to the project directory
 cd /home/ec2-user/project
 
-# Stop and remove any existing containers
-docker-compose down
+# # Stop and remove any existing containers
+# docker-compose down
 
 # Build and start the containers in detached mode
 docker-compose up --build -d
